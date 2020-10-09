807ead97e35b17bf49e115de50a4d7438f509d99
Nandor Licker <n@ndor.email>
2020-10-09 15:25:48 +0100

    [llir-clang] Fixed LLIR data layout

--- clang/lib/Basic/Targets/LLIR.h
+++ clang/lib/Basic/Targets/LLIR.h
@@ -32,7 +32,7 @@ class LLVM_LIBRARY_VISIBILITY LLIR_X86_64TargetInfo : public X86_64TargetInfo {
       : X86_64TargetInfo(Triple, Opts) {
     // Pointers are 32-bit in x32.
     resetDataLayout(
-        "e-m:g-p270:32:32-p271:32:32-p272:64:"
+        "e-m:e-p270:32:32-p271:32:32-p272:64:"
         "64-i64:64-f80:128-n8:16:32:64-S128");
   }
 
