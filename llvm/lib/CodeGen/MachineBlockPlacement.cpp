d21a1521ae507459bee8c19edcc2154f7dc70a6f
Nandor Licker <n@ndor.email>
2020-10-30 00:34:34 +0000

    [llir-llvm] Skip block placement on OCaml functions

--- llvm/lib/CodeGen/MachineBlockPlacement.cpp
+++ llvm/lib/CodeGen/MachineBlockPlacement.cpp
@@ -3293,6 +3293,9 @@ bool MachineBlockPlacement::runOnMachineFunction(MachineFunction &MF) {
   if (skipFunction(MF.getFunction()))
     return false;
 
+  if (MF.getFunction().getCallingConv() == CallingConv::LLIR_CAML)
+    return false;
+
   // Check for single-block functions and skip them.
   if (std::next(MF.begin()) == MF.end())
     return false;
