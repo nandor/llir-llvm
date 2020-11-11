d36060c953f8e1755aeeb2976b0819e1a40044f6
Nandor Licker <n@ndor.email>
2020-11-11 23:25:01 +0000

    [llir-compiler-rt] Disabled clear_cache

--- compiler-rt/lib/builtins/clear_cache.c
+++ compiler-rt/lib/builtins/clear_cache.c
@@ -7,10 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "int_lib.h"
-#ifndef __llir__
-#ifndef NDEBUG
 #include <assert.h>
-#endif
 #include <stddef.h>
 
 #if __APPLE__
@@ -88,9 +85,7 @@ void __clear_cache(void *start, void *end) {
   __asm __volatile("svc 0x0"
                    : "=r"(start_reg)
                    : "r"(syscall_nr), "r"(start_reg), "r"(end_reg), "r"(flags));
-#ifndef NDEBUG
   assert(start_reg == 0 && "Cache flush syscall failed.");
-#endif
 #else
   compilerrt_abort();
 #endif
@@ -167,9 +162,7 @@ void __clear_cache(void *start, void *end) {
   __asm __volatile("ecall"
                    : "=r"(start_reg)
                    : "r"(start_reg), "r"(end_reg), "r"(flags), "r"(syscall_nr));
-#ifndef NDEBUG
   assert(start_reg == 0 && "Cache flush syscall failed.");
-#endif
 #else
 #if __APPLE__
   // On Darwin, sys_icache_invalidate() provides this functionality
@@ -179,4 +172,3 @@ void __clear_cache(void *start, void *end) {
 #endif
 #endif
 }
-#endif
