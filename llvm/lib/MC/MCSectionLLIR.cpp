9bbeeac79c8a26ee6bc7a2c9331c579e50fa54be
Nandor Licker <n@ndor.email>
2020-12-03 22:01:06 +0000

    [llir-llvm] Fixed section name printing

--- llvm/lib/MC/MCSectionLLIR.cpp
+++ llvm/lib/MC/MCSectionLLIR.cpp
@@ -18,13 +18,30 @@ using namespace llvm;
 
 MCSectionLLIR::~MCSectionLLIR() {} // anchor.
 
+static void printName(raw_ostream &OS, StringRef Name) {
+  OS << '"';
+  for (const char *B = Name.begin(), *E = Name.end(); B < E; ++B) {
+    if (*B == '"') // Unquoted "
+      OS << "\\\"";
+    else if (*B != '\\') // Neither " or backslash
+      OS << *B;
+    else if (B + 1 == E) // Trailing backslash
+      OS << "\\\\";
+    else {
+      OS << B[0] << B[1]; // Quoted character
+      ++B;
+    }
+  }
+  OS << '"';
+}
+
 void MCSectionLLIR::PrintSwitchToSection(
     const MCAsmInfo &MAI,
     const Triple &T,
     raw_ostream &OS,
     const MCExpr *Subsection) const
 {
-  OS << ".section\t" << Name << "\n";
+  OS << ".section\t"; printName(OS, Name); OS << "\n";
 }
 
 bool MCSectionLLIR::UseCodeAlign() const { return false; }
